80ee4363e764732353522bec8422ed1d30ced2ee
Aeronaut <aeronaut@MacBook-Pro-Aeronaut-2.local>
2017-11-04 20:04:51 +0100

    Examples 18.2

---
+++ src/main/java/com/crud/tasks/controller/TaskController.java
@@ -0,0 +1,46 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TaskDto;
+import com.crud.tasks.mapper.TaskMapper;
+import com.crud.tasks.service.DbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
+@RestController
+@RequestMapping("/v1/task")
+public class TaskController {
+    @Autowired
+    private DbService service;
+    @Autowired
+    private TaskMapper taskMapper;
+
+    @RequestMapping(method = RequestMethod.GET, value = "getTasks")
+    public List<TaskDto> getTasks() {
+        return taskMapper.mapToTaskDtoList(service.getAllTasks());
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/{taskId}")
+    public TaskDto getTaskById(@PathVariable String taskId) throws TaskNotFoundException {
+        return taskMapper.mapToTaskDto(service.getTask(Long.parseLong(taskId)).orElseThrow(TaskNotFoundException::new));
+    }
+
+    @RequestMapping(method = RequestMethod.DELETE, value = "/{taskId}")
+    public void deleteTask(@PathVariable String taskId) {
+        service.deleteTask(Long.parseLong(taskId));
+    }
+
+    @RequestMapping(method = RequestMethod.PUT, value = "updateTask")
+    public TaskDto updateTask(@RequestBody TaskDto taskDto) {
+        return taskMapper.mapToTaskDto(service.saveTask(taskMapper.mapToTask(taskDto)));
+    }
+
+    @RequestMapping(method = RequestMethod.POST, value = "createTask", consumes = APPLICATION_JSON_VALUE)
+    public void createTask(@RequestBody TaskDto taskDto) {
+        service.saveTask(taskMapper.mapToTask(taskDto));
+    }
+
+}
---
+++ src/main/java/com/crud/tasks/domain/Task.java
@@ -0,0 +1,24 @@
+package com.crud.tasks.domain;
+
+import lombok.Getter;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Entity(name = "tasks")
+public class Task {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String title;
+
+    @Column(name = "description")
+    private String content;
+}
